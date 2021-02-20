56c2f139586eec5558f93ac2a9111fb87316ed60
tutkuince <tutku.ince@outlook.com>
2021-02-20 17:37:49 +0300

    initial commit

---
+++ src/main/java/digital/osf/geocodestoaddress/GeocodesToAddressApplication.java
@@ -0,0 +1,13 @@
+package digital.osf.geocodestoaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodesToAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocodesToAddressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/digital/osf/geocodestoaddress/GeocodesToAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.osf.geocodestoaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodesToAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
